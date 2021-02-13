4b7ae6c957e8977eec7fd51dbe3c90863078d3aa
aschamberger <mail@andreass.net>
2021-02-13 20:37:30 +0100

    Add files via upload

---
+++ hermesAudioPlayer/hermes.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+MQTT=${MQTT:-localhost:1883}
+LMS=${LMS:-localhost:9000}
+SITE_ID=${SITE_ID:-default}
+OUTPUT_DEVICE=${OUTPUT_DEVICE:-default}
+MAC_ADDRESS=${MAC_ADDRESS:-02:00:00:00:00:00}
+GPIO=${GPIO:-;;}
+
+# run squeezelite with user squeezelite
+exec su-exec hermes python3 /usr/local/bin/hermes.py "$MQTT" "$LMS" "$SITE_ID" "$OUTPUT_DEVICE" "$MAC_ADDRESS" "$GPIO"
