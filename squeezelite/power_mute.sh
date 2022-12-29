600de00b4dc1118ad23764e224ffc37c19da2c23
aschamberger <mail@andreass.net>
2022-12-29 21:32:36 +0100

    change usb + fix echos

--- squeezelite/power_mute.sh
+++ squeezelite/power_mute.sh
@@ -7,8 +7,6 @@
 #   1: on
 #   2: init
 
-#echo "power/mute ..."
-
 # create lock in order to make sure we have exclusive access to GPIO
 exec 200>/var/lock/gpio || exit 1
 flock 200 || exit 1
@@ -16,14 +14,14 @@ flock 200 || exit 1
 case $1 in
     # init
     2)
-        echo "init $OUTPUT_DEVICE:\n"
+        echo -n "init $OUTPUT_DEVICE: "
         if [[ -n "$GPIO_PSU_RELAY" ]]; then
             # set output mode if not initialized yet
             RELAY_MODE=$(gpio get_mode $GPIO_PSU_RELAY)
             if [[ $RELAY_MODE == 0 ]]; then
                 gpio set_mode $GPIO_PSU_RELAY 1
                 gpio write $GPIO_PSU_RELAY 0
-                echo "PSU relay: $GPIO_PSU_RELAY ..."
+                echo -n "PSU relay: $GPIO_PSU_RELAY ..."
             fi
         fi
         if [[ -n "$GPIO_SPS" ]]; then
@@ -32,7 +30,7 @@ case $1 in
             if [[ $SPEAKER_SWITCHER_MODE == 0 ]]; then
                 gpio set_mode $GPIO_SPS 1
                 gpio write $GPIO_SPS 0
-                echo "SPS: $GPIO_SPS ..."
+                echo -n "SPS: $GPIO_SPS ..."
             fi
         fi
         if [[ -n "$GPIO_MUTE" ]]; then
@@ -41,7 +39,7 @@ case $1 in
             if [[ $MUTE_MODE == 0 ]]; then
                 gpio set_mode $GPIO_MUTE 1
                 gpio write $GPIO_MUTE 1
-                echo "Mute: $GPIO_MUTE ..."
+                echo -n "Mute: $GPIO_MUTE ..."
             fi
         fi
         if [[ -n "$GPIO_SHUTDOWN" ]]; then
@@ -50,56 +48,56 @@ case $1 in
             if [[ $SHUTDOWN_MODE == 0 ]]; then
                 gpio set_mode $GPIO_SHUTDOWN 1
                 gpio write $GPIO_SHUTDOWN 1
-                echo "Shutdown: $GPIO_SHUTDOWN ..."
+                echo -n "Shutdown: $GPIO_SHUTDOWN ..."
             fi
         fi
-        echo "\n"
+        echo ""
         ;;
     # on
     1)
-        echo "power on $OUTPUT_DEVICE:\n"
+        echo -n "power on $OUTPUT_DEVICE: "
         if [[ -n "$GPIO_PSU_RELAY" ]]; then
             RELAY_ON=$(gpio read $GPIO_PSU_RELAY)
             if [[ $RELAY_ON == 0 ]]; then
                 gpio write $GPIO_PSU_RELAY 1
-                echo "PSU relay: $GPIO_PSU_RELAY ..."
+                echo -n "PSU relay: $GPIO_PSU_RELAY ..."
                 sleep 2
             fi
         fi
         if [[ -n "$GPIO_SPS" ]]; then
             gpio write $GPIO_SPS 1
-            echo "SPS: $GPIO_SPS ..."
+            echo -n "SPS: $GPIO_SPS ..."
         fi
         if [[ -n "$GPIO_SHUTDOWN" ]]; then
             gpio write $GPIO_SHUTDOWN 0
-            echo "Shutdown: $GPIO_SHUTDOWN ..."
+            echo -n "Shutdown: $GPIO_SHUTDOWN ..."
         fi
         if [[ -n "$GPIO_MUTE" ]]; then
             gpio write $GPIO_MUTE 0
-            echo "Mute: $GPIO_MUTE ..."
+            echo -n "Mute: $GPIO_MUTE ..."
         fi
         if [[ -n "$HASS_SWITCH" ]]; then
             curl -s -X POST -H "Authorization: Bearer $HASS_BEARER" \
                 -H "Content-Type: application/json" \
                 -d '{"entity_id": "'"$HASS_SWITCH"'"}' \
                 http://$HASS_HOST/api/services/switch/turn_on
-            echo "HASS switch: $HASS_SWITCH ..."
+            echo -n "HASS switch: $HASS_SWITCH ..."
         fi
-        echo "\n"
+        echo ""
         ;;
     # off
     0)
-        echo "power off $OUTPUT_DEVICE:\n"
+        echo -n "power off $OUTPUT_DEVICE: "
         if [[ -n "$HASS_SWITCH" ]]; then
             curl -s -X POST -H "Authorization: Bearer $HASS_BEARER" \
                 -H "Content-Type: application/json" \
                 -d '{"entity_id": "'"$HASS_SWITCH"'"}' \
                 http://$HASS_HOST/api/services/switch/turn_off
-            echo "HASS switch: $HASS_SWITCH ..."
+            echo -n "HASS switch: $HASS_SWITCH ..."
         fi
         if [[ -n "$GPIO_MUTE" ]]; then
             if [[ -n "$GPIO_AMP_MUTE_ON_PLAYERS" ]]; then
-                echo "Mute on players ..."
+                echo -n "Mute on players ..."
                 ALL_OFF=1
                 IFS=\;
                 for token in $GPIO_AMP_MUTE_ON_PLAYERS; do
@@ -109,7 +107,7 @@ case $1 in
                         POWER=$(curl -s -H 'Content-Type: application/json' -d "$DATA" http://$LMS_HOST/jsonrpc.js)
                         # empty result if player not registered
                         if [[ -n "$POWER" ]]; then
-                            PLAYER_ON=$(echo $POWER | jq -r '.result._power' )
+                            PLAYER_ON=$(echo -n $POWER | jq -r '.result._power' )
                             if [[ $PLAYER_ON == 1 ]]; then
                                 ALL_OFF=0
                                 break
@@ -119,31 +117,31 @@ case $1 in
                 done
                 if [[ $ALL_OFF == 1 ]]; then
                     gpio write $GPIO_MUTE 1
-                    echo "Mute: $GPIO_MUTE ..."
+                    echo -n "Mute: $GPIO_MUTE ..."
                 fi
             else
-                echo "Mute on GPIO ..."
+                echo -n "Mute on GPIO ..."
                 gpio write $GPIO_MUTE 1
-                echo "Mute: $GPIO_MUTE ..."
+                echo -n "Mute: $GPIO_MUTE ..."
             fi
         fi
         if [[ -n "$GPIO_SHUTDOWN" ]]; then
             if [[ -n "$GPIO_AMP_SHUTDOWN_ON_AMP_MUTE" ]]; then
-                echo "Shutdown on mute ($GPIO_AMP_SHUTDOWN_ON_AMP_MUTE) ..."
+                echo -n "Shutdown on mute ($GPIO_AMP_SHUTDOWN_ON_AMP_MUTE) ..."
                 GPIO_ON=$(gpio read $GPIO_AMP_SHUTDOWN_ON_AMP_MUTE)
                 if [[ $GPIO_ON == 1 ]]; then
                     gpio write $GPIO_SHUTDOWN 1
-                    echo "Shutdown: $GPIO_SHUTDOWN ..."
+                    echo -n "Shutdown: $GPIO_SHUTDOWN ..."
                 fi
             else
-                echo "Shutdown on GPIO ..."
+                echo -n "Shutdown on GPIO ..."
                 gpio write $GPIO_SHUTDOWN 1
-                echo "Shutdown: $GPIO_SHUTDOWN ..."
+                echo -n "Shutdown: $GPIO_SHUTDOWN ..."
             fi
         fi
         if [[ -n "$GPIO_SPS" ]]; then
             gpio write $GPIO_SPS 0
-            echo "SPS: $GPIO_SPS ..."
+            echo -n "SPS: $GPIO_SPS ..."
         fi
         if [[ -n "$GPIO_PSU_RELAY" ]]; then
             sleep 5
@@ -160,10 +158,10 @@ case $1 in
             done
             if [[ $ALL_OFF == 1 ]]; then
                 gpio write $GPIO_PSU_RELAY 0
-                echo "PSU relay: $GPIO_PSU_RELAY ..."
+                echo -n "PSU relay: $GPIO_PSU_RELAY ..."
             fi
         fi
-        echo "\n"
+        echo ""
         ;;
 esac
 
