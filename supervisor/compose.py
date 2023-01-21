1aed8a3220c95b754f7e7718bae6f57414f9332f
aschamberger <mail@andreass.net>
2023-01-21 17:26:53 +0100

    Update compose.py

--- supervisor/compose.py
+++ supervisor/compose.py
@@ -7,14 +7,14 @@ envFile = "/etc/opt/compose/.env"
 composeFile = "/usr/local/src/sma/compose.yml"
 
 def up():
-    p = subprocess.run( [ 'docker-compose', '-f', composeFile, '--profile', 'on', 'up', '-d' ], capture_output=True, text=True )
+    p = subprocess.run( [ 'docker-compose', '-f', composeFile, '--env-file', envFile, '--profile', 'on', 'up', '-d' ], capture_output=True, text=True )
     if p.returncode == 0: 
         print(p.stdout)
     else:
         print("error")
 
 def upRecreate():
-    p = subprocess.run( [ 'docker-compose', '-f', composeFile, '--profile', 'on', 'up', '--force-recreate', '-d' ], capture_output=True, text=True )
+    p = subprocess.run( [ 'docker-compose', '-f', composeFile, '--env-file', envFile, '--profile', 'on', 'up', '--force-recreate', '-d' ], capture_output=True, text=True )
     if p.returncode == 0: 
         print(p.stdout)
     else:
@@ -22,8 +22,7 @@ def upRecreate():
 
 def readConfigValue(key):
     return get_key(envFile, key)
-            
+
 def updateConfigValue(key, value):
     #success, key, value = set_key(file, key, value, quote, export)
     return set_key(envFile, key, value, 'never', false)
-    
