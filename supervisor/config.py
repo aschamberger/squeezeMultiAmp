957b4129027a43378aae4c452f62c553f482ae86
aschamberger <mail@andreass.net>
2023-02-16 21:54:38 +0100

    Update config.py

--- supervisor/config.py
+++ supervisor/config.py
@@ -2,7 +2,7 @@
 
 import re, uuid
 
-eqChannels = ["00. 31 Hz",
+eq_channels = ["00. 31 Hz",
     "01. 63 Hz",
     "02. 125 Hz",
     "03. 250 Hz",
@@ -12,8 +12,9 @@ eqChannels = ["00. 31 Hz",
     "07. 4 kHz",
     "08. 8 kHz",
     "09. 16 kHz"]
+
 # equalizer range is from 36 to 95
-eqPresets = {
+eq_presets = {
     "flat": ['66', '66', '66', '66', '66', '66', '66', '66', '66', '66'],
     "classic": ['78', '70', '67', '63', '58', '60', '64', '69', '78', '76'],
     "rock": ['76', '78', '75', '70', '67', '72', '72', '72', '69', '69'],
@@ -21,272 +22,221 @@ eqPresets = {
     "pop": ['79', '75', '70', '67', '64', '72', '70', '73', '76', '79']
 }
 
-mac = ':'.join(re.findall('..', '%012x' % uuid.getnode())) # mac address
+discovery_prefix = "homeassistant"
+node_id = f"sma{''.join(re.findall('..', '%012x' % uuid.getnode()))}"
+
+subscriptions = [
+    f"{discovery_prefix}/+/{node_id}/+/do",
+    f"{discovery_prefix}/+/{node_id}/+/set"
+]
 
 device = {
     "name": "sqeezeMultiAmp",
-    "identifiers": mac
+    "identifiers": node_id
 }
 
 # set up home assisstant entities
 # device: sqeezeMultiAmp
 entities = [
     {
-        'topic': "homeassistant/binary_sensor/sma_supervisor/config",
-        'payload':
-        {
-            "name": "sMA Supervisor State",
-            "device": device,
-            "entity_category": "diagnostic",
-            "dev_cla": "running",
-            "stat_t": "homeassistant/binary_sensor/sma_supervisor/state"
-        }
+        "~": f"{discovery_prefix}/binary_sensor/{node_id}/{node_id}_supervisor",
+        "name": "sMA Supervisor Container State",
+        "device": device,
+        "entity_category": "diagnostic",
+        "dev_cla": "running",
+        "stat_t": "~/state"
     },
     {
-        'topic': "homeassistant/button/sma_shutdown/config",
-        'payload':
-        {
-            "name": "sMA Server Shutdown",
-            "device": device,
-            "entity_category": "config",
-            "dev_cla": "None",
-            "icon": "mdi:stop",
-            "cmd_t": "homeassistant/button/sma_shutdown/do"
-        }
+        "~": f"{discovery_prefix}/button/{node_id}/{node_id}_shutdown",
+        "name": "sMA Server Shutdown",
+        "device": device,
+        "entity_category": "config",
+        "dev_cla": "None",
+        "icon": "mdi:stop",
+        "cmd_t": "~/do"
     },
     {
-        'topic': "homeassistant/button/sma_restart/config",
-        'payload':
-        {
-            "name": "sMA Server Restart",
-            "device": device,
-            "entity_category": "config",
-            "dev_cla": "restart",
-            "icon": "mdi:restart",
-            "cmd_t": "homeassistant/button/sma_restart/do"
-        }
+        "~": f"{discovery_prefix}/button/{node_id}/{node_id}_restart",
+        "name": "sMA Server Restart",
+        "device": device,
+        "entity_category": "config",
+        "dev_cla": "restart",
+        "icon": "mdi:restart",
+        "cmd_t": "~/do"
     },
     {
-        'topic': "homeassistant/button/sma_compose_recreate/config",
-        'payload':
-        {
-            "name": "sMA Container Recreate",
-            "device": device,
-            "entity_category": "config",
-            "dev_cla": "restart",
-            "icon": "mdi:autorenew",
-            "cmd_t": "homeassistant/button/sma_compose_recreate/do"
-        }
+        "~": f"{discovery_prefix}/button/{node_id}/{node_id}_compose_recreate",
+        "name": "sMA Container Recreate",
+        "device": device,
+        "entity_category": "config",
+        "dev_cla": "restart",
+        "icon": "mdi:autorenew",
+        "cmd_t": "~/do"
     },
     {
-        'topic': "homeassistant/text/sma_lms_host/config",
-        'payload':
-        {
-            "name": "sMA Logitech Media Server Host",
-            "description": "Format: host:port",
-            "device": device,
-            "entity_category": "config",
-            "icon": "mdi:server-network",
-            "cmd_t": "homeassistant/text/sma_lms_host/set",
-            "stat_t": "homeassistant/text/sma_lms_host/state"
-        }
+        "~": f"{discovery_prefix}/text/{node_id}/{node_id}_lms_host",
+        "name": "sMA Logitech Media Server Host",
+        "description": "Format: host:port",
+        "device": device,
+        "entity_category": "config",
+        "icon": "mdi:server-network",
+        "cmd_t": "~/set",
+        "stat_t": "~/state"
     },
     {
-        'topic': "homeassistant/text/sma_mqtt_host/config",
-        'payload':
-        {
-            "name": "sMA MQTT Host",
-            "description": "Format: host:port",
-            "device": device,
-            "entity_category": "config",
-            "icon": "mdi:server-network",
-            "cmd_t": "homeassistant/text/sma_mqtt_host/set",
-            "stat_t": "homeassistant/text/sma_mqtt_host/state"
-        }
+        "~": f"{discovery_prefix}/text/{node_id}/{node_id}_mqtt_host",
+        "name": "sMA MQTT Host",
+        "description": "Format: host:port",
+        "device": device,
+        "entity_category": "config",
+        "icon": "mdi:server-network",
+        "cmd_t": "~/set",
+        "stat_t": "~/state"
     },
     {
-        'topic': "homeassistant/text/sma_hass_host/config",
-        'payload':
-        {
-            "name": "sMA Home Assistant Host",
-            "description": "Format: host:port",
-            "device": device,
-            "entity_category": "config",
-            "icon": "mdi:server-network",
-            "cmd_t": "homeassistant/text/sma_hass_host/set",
-            "stat_t": "homeassistant/text/sma_hass_host/state"
-        }
+        "~": f"{discovery_prefix}/text/{node_id}/{node_id}_hass_host",
+        "name": "sMA Home Assistant Host",
+        "description": "Format: host:port",
+        "device": device,
+        "entity_category": "config",
+        "icon": "mdi:server-network",
+        "cmd_t": "~/set",
+        "stat_t": "~/state"
     },
     {
-        'topic': "homeassistant/text/sma_hass_bearer/config",
-        'payload':
-        {
-            "name": "sMA Home Assistant bearer token",
-            "device": device,
-            "entity_category": "config",
-            "icon": "mdi:lock",
-            "mode": "password",
-            "cmd_t": "homeassistant/text/sma_hass_bearer/set",
-            "stat_t": "homeassistant/text/sma_hass_bearer/state"
-        }
+        "~": f"{discovery_prefix}/text/{node_id}/{node_id}_hass_bearer",
+        "name": "sMA Home Assistant bearer token",
+        "device": device,
+        "entity_category": "config",
+        "icon": "mdi:lock",
+        "mode": "password",
+        "cmd_t": "~/set",
+        "stat_t": "~/state"
     },
     {
-        'topic': "homeassistant/button/sma_remote_backup/config",
-        'payload':
-        {
-            "name": "sMA Remote Backup ",
-            "device": device,
-            "entity_category": "config",
-            "icon": "mdi:cloud-upload",
-            "cmd_t": "homeassistant/button/sma_remote_backup/do"
-        }
+        "~": f"{discovery_prefix}/button/{node_id}/{node_id}_remote_backup",
+        "name": "sMA Remote Backup ",
+        "device": device,
+        "entity_category": "config",
+        "icon": "mdi:cloud-upload",
+        "cmd_t": "~/do"
     },
     {
-        'topic': "homeassistant/text/sma_backup_host/config",
-        'payload':
-        {
-            "name": "sMA Remote Backup Host",
-            "description": "Format: user@host:port",
-            "device": device,
-            "entity_category": "config",
-            "icon": "mdi:server-network",
-            "cmd_t": "homeassistant/text/sma_backup_host/set",
-            "stat_t": "homeassistant/text/sma_backup_host/state"
-        }
+        "~": f"{discovery_prefix}/text/{node_id}/{node_id}_backup_host",
+        "name": "sMA Remote Backup Host",
+        "description": "Format: user@host:port",
+        "device": device,
+        "entity_category": "config",
+        "icon": "mdi:server-network",
+        "cmd_t": "~/set",
+        "stat_t": "~/state"
     },
     {
-        'topic': "homeassistant/text/sma_backup_password/config",
-        'payload':
-        {
-            "name": "sMA Remote Backup Password",
-            "device": device,
-            "entity_category": "config",
-            "icon": "mdi:lock",
-            "mode": "password",
-            "cmd_t": "homeassistant/text/sma_backup_password/set",
-            "stat_t": "homeassistant/text/sma_backup_password/state"
-        }
+        "~": f"{discovery_prefix}/text/{node_id}/{node_id}_backup_password",
+        "name": "sMA Remote Backup Password",
+        "device": device,
+        "entity_category": "config",
+        "icon": "mdi:lock",
+        "mode": "password",
+        "cmd_t": "~/set",
+        "stat_t": "~/state"
     },
     {
-        'topic': "homeassistant/text/sma_backup_folder/config",
-        'payload':
-        {
-            "name": "sMA Remote Backup Folder",
-            "device": device,
-            "entity_category": "config",
-            "icon": "mdi:folder",
-            "cmd_t": "homeassistant/text/sma_backup_folder/set",
-            "stat_t": "homeassistant/text/sma_backup_folder/state"
-        }
+        "~": f"{discovery_prefix}/text/{node_id}/{node_id}_backup_folder",
+        "name": "sMA Remote Backup Folder",
+        "device": device,
+        "entity_category": "config",
+        "icon": "mdi:folder",
+        "cmd_t": "~/set",
+        "stat_t": "~/state"
     },
     # {
-        # 'topic': "homeassistant/number/sma_gpio_relay/config",
-        # 'payload':
-        # {
-            # "name": "sMA GPIO Relay",
-            # "device": device,
-            # "entity_category": "config",
-            # "icon": "mdi:power-plug",
-            # "cmd_t": "homeassistant/number/sma_gpio_relay/set",
-            # "stat_t": "homeassistant/number/sma_gpio_relay/state",
-            # "min": 1,
-            # "max": 26
-        # }
+        # "~": f"{discovery_prefix}/number/{node_id}/{node_id}_gpio_relay/",
+        # "name": "sMA GPIO Relay",
+        # "device": device,
+        # "entity_category": "config",
+        # "icon": "mdi:power-plug",
+        # "cmd_t": "~/set",
+        # "stat_t": "~/state",
+        # "min": 1,
+        # "max": 26
     # }
 ]
 
 # subdevice: sqeezeMultiAmp Channel #?
 for channel in range(1, 9):
-    channel = "{:02d}".format(channel)
+    channel = f"{channel:02d}"
     subdevice = {
-        "name": "sqeezeMultiAmp Channel #" + channel,
-        "identifiers": "02:00:00:00:00:" + channel,
-        "via_device": mac
+        "name": f"sqeezeMultiAmp Channel #{channel}",
+        "identifiers": f"{node_id}-ch{channel}",
+        "via_device": node_id
     }
 
     entities += [
         {
-            'topic': "homeassistant/binary_sensor/sma_channel_" + channel + "/config",
-            'payload':
-            {
-                "name": "sMA Channel #" + channel + " State",
-                "device": subdevice,
-                "entity_category": "diagnostic",
-                "dev_cla": "running",
-                "stat_t": "homeassistant/binary_sensor/sma_channel_" + channel + "/state"
-            }
+            "~": f"{discovery_prefix}/binary_sensor/{node_id}/{node_id}_ch{channel}",
+            "name": f"sMA Channel #{channel} Container State",
+            "device": subdevice,
+            "entity_category": "diagnostic",
+            "dev_cla": "running",
+            "stat_t": "~/state"
         },
         {
-            'topic': "homeassistant/text/sma_channel_" + channel + "_player_name/config",
-            'payload':
-            {
-                "name": "sMA Channel #" + channel + " Player Name",
-                "device": subdevice,
-                "entity_category": "config",
-                "icon": "mdi:rename",
-                "dev_cla": "running",
-                "cmd_t": "homeassistant/text/sma_channel_" + channel + "_player_name/set",
-                "stat_t": "homeassistant/text/sma_channel_" + channel + "_player_name/state"
-            }
+            "~": f"{discovery_prefix}/text/{node_id}/{node_id}_ch{channel}_player_name",
+            "name": f"sMA Channel #{channel} Player Name",
+            "device": subdevice,
+            "entity_category": "config",
+            "icon": "mdi:rename",
+            "dev_cla": "running",
+            "cmd_t": "~/set",
+            "stat_t": "~/state"
         }
     ]
 
-    for eqChannel in eqChannels:
+    for eq_channel in eq_channels:
         entities.append({
-            'topic': "homeassistant/number/sma_channel_" + channel + "_eq_" + eqChannel[:2] + "/config",
-            'payload':
-            {
-                "name": "sMA Channel #" + channel + " EQ " + eqChannel,
-                "device": subdevice,
-                "entity_category": "config",
-                "icon": "mdi:tune",
-                "cmd_t": "homeassistant/number/sma_channel_" + channel + "_eq_" + eqChannel[:2] + "/set",
-                "stat_t": "homeassistant/number/sma_channel_" + channel + "_eq_" + eqChannel[:2] + "/state",
-                "min": 36,
-                "max": 95
-            }
+            "~": f"{discovery_prefix}/number/{node_id}/{node_id}_ch{channel}_eq{eq_channel[:2]}_eqsetting",
+            "name": f"sMA Channel #{channel} EQ {eq_channel} setting",
+            "device": subdevice,
+            "entity_category": "config",
+            "icon": "mdi:tune",
+            "cmd_t": "~/set",
+            "stat_t": "~/state",
+            "min": 36,
+            "max": 95
         })
 
-    for eqPreset in eqPresets.keys():
+    for eq_preset in eq_presets.keys():
         entities.append({
-            'topic': "homeassistant/scene/sma_channel_" + channel + "_eq_preset_" + eqPreset + "/config",
-            'payload':
-            {
-                "name": "sMA Channel #" + channel + " EQ preset " + eqPreset,
-                "device": subdevice,
-                "dev_cla": "None",
-                "icon": "mdi:folder-star",
-                "cmd_t": "homeassistant/scene/sma_channel_" + channel + "_eq_" + eqPreset + "/set",
-                "pl_on": eqPreset
-            }
+            "~": f"{discovery_prefix}/scene/{node_id}/{node_id}_ch{channel}_eqpreset_{eq_preset}",
+            "name": f"sMA Channel #{channel} EQ preset {eq_preset}",
+            "device": subdevice,
+            "dev_cla": "None",
+            "icon": "mdi:folder-star",
+            "cmd_t": f"{discovery_prefix}/scene/{node_id}/{node_id}_ch{channel}_eqpreset/set",
+            "pl_on": eq_preset
         })
 
     entities += [
         {
-            'topic': "homeassistant/number/sma_channel_" + channel + "_volume/config",
-            'payload':
-            {
-                "name": "sMA Channel #" + channel + " volume",
-                "device": subdevice,
-                "entity_category": "config",
-                "icon": "mdi:volume-high",
-                "cmd_t": "homeassistant/number/sma_channel_" + channel + "_volume/set",
-                "stat_t": "homeassistant/number/sma_channel_" + channel + "_volume/state",
-                "min": 0,
-                "max": 100
-            }
+            "~": f"{discovery_prefix}/number/{node_id}/{node_id}_ch{channel}_volume",
+            "name": f"sMA Channel #{channel} volume",
+            "device": subdevice,
+            "entity_category": "config",
+            "icon": "mdi:volume-high",
+            "cmd_t": "~/set",
+            "stat_t": "~/state",
+            "min": 0,
+            "max": 100
         },
         {
-            'topic': "homeassistant/text/sma_channel_" + channel + "_hass_switch/config",
-            'payload':
-            {
-                "name": "sMA Channel #" + channel + " External Switch",
-                "description": "Home Assistant entity id that should be switched on/off based on player state",
-                "device": subdevice,
-                "entity_category": "config",
-                "icon": "mdi:electric-switch",
-                "cmd_t": "homeassistant/text/sma_channel_" + channel + "_hass_switch/set",
-                "stat_t": "homeassistant/text/sma_channel_" + channel + "_hass_switch/state"
-            }
+            "~": f"{discovery_prefix}/text/{node_id}/{node_id}_ch{channel}_hass_switch",
+            "name": f"sMA Channel #{channel} external switch",
+            "description": "Home Assistant entity id that should be switched on/off based on player state",
+            "device": subdevice,
+            "entity_category": "config",
+            "icon": "mdi:electric-switch",
+            "cmd_t": "~/set",
+            "stat_t": "~/state"
         }
     ]
